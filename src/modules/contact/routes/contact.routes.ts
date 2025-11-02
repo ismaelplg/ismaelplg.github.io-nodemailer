59b5d588a1faaa8686d23bb0b451d4c305dbed30
Ismael Lopez <ismaelplg@hotmail.com>
2025-11-02 15:54:13 -0600

    chore: fix contact controller path

--- src/modules/contact/routes/contact.routes.ts
+++ src/modules/contact/routes/contact.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { sendContactEmail } from '../controllers/contact.controller';
+import { sendContactEmail } from '../controllers/contact.controller.js';
 
 const router = Router();
 
